b7e0964b13501854f2ac39868cc6132e84515c6c
Elgatito145 <luismiguelcr110@gmail.com>
2020-08-04 23:30:19 -0600

    packages json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule,LOCALE_ID } from '@angular/core';
 import { AppComponent } from './app.component';
+import {HeaderComponent} from './header/header.componet';
+import{FooterComponent} from './footer/footer.component';
+import { DirectivaComponent } from './directiva/directiva.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import {ClienteService} from './clientes/cliente.service';
+import { RouterModule, Routes} from '@angular/router';
+import { HttpClientModule} from '@angular/common/http';
+import { FormComponent } from './clientes/form.component';
+import {FormsModule} from '@angular/forms';
+import localeES from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { PaginatorComponent } from './paginator/paginator.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+registerLocaleData(localeES,'es');
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+
+const routes: Routes = [
+   {path: '', redirectTo: '/clientes', pathMatch: 'full'},
+   {path: 'directivas', component: DirectivaComponent},
+   {path: 'clientes', component: ClientesComponent },
+   { path: 'clientes/page/:page', component: ClientesComponent },
+   {path: 'clientes/form', component: FormComponent},
+  {path: 'clientes/form/:id', component: FormComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DirectivaComponent,
+    ClientesComponent,
+    FormComponent,
+    PaginatorComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatMomentDateModule
   ],
-  providers: [],
+  providers: [ClienteService,{provide: LOCALE_ID, useValue: 'es' }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
